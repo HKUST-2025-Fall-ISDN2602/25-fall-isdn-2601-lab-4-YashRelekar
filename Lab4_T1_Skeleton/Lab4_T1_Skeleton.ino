0a18511ad77c4b81bd01c8302323610d5670a39a
Yash <ymrelekar@connect.ust.hk>
2025-12-07 11:24:07 +0800

    Lab 4
    Task 1

--- Lab4_T1_Skeleton/Lab4_T1_Skeleton.ino
+++ Lab4_T1_Skeleton/Lab4_T1_Skeleton.ino
@@ -1,30 +1,31 @@
-/*Change all the ? in the code to Control the direction of rotation based on whether your selected pin is touched.*/
-
-#define IN1    ?  // Replace the ? with the GPIO pin you selected to connect IN1
-#define IN2    ?   // Replace the ? with the GPIO pin you selected to connect IN2
-#define TOUCH_PIN ?  // Replace the ? with the GPIO touch pin you selected 
-#define THRESHOLD ?   // Replace the ? with the reading when the pin is touched from Lab 3
+#define IN1        18      // GPIO connected to L298N IN1
+#define IN2        19      // GPIO connected to L298N IN2
+#define TOUCH_PIN  T6      
+#define THRESHOLD  30      
 
 int touch_reading;
 
 void setup() {
-  pinMode(?, ?);  // Set IN1 as an output pin
-  pinMode(?, ?);  // Set IN2 as an output pin
-  
-  pinMode(?, ?); // Set TOUCH_PIN as an output pin
+  // IN1 and IN2 as output pins
+  pinMode(IN1, OUTPUT);  
+  pinMode(IN2, OUTPUT);  
+  pinMode(14, INPUT);   // 14 corresponds to T6 
 }
 
 void loop() {
-  touch_reading = touchRead(?); // Read the touch sensor
+  // Read the touch sensor
+  touch_reading = touchRead(TOUCH_PIN); 
   
-  /* if TOUCH_PIN is touched, then rotate the shaft in clockwise
-     else TOUCH_PIN is not touched, then rotate the shaft in counterclockwise */
-  
-  if (touch_reading < THRESHOLD){
-     digitalWrite(?, ?);
-     digitalWrite(?, ?);  
-  }else{
-     digitalWrite(?, ?);
-     digitalWrite(?, ?);  
+  /* if TOUCH_PIN is touched, shaft rotates clockwise
+     else TOUCH_PIN is not touched, shaft rotates counterclockwise */
+
+  if (touch_reading < THRESHOLD) {
+    // Touched → forward (clockwise)
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);  
+  } else {
+    // Not touched → backward (counterclockwise)
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);  
   }
 }
